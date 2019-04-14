1b97eba0b73a603af7dcc0c9a24f9db1863d12bc
Lewis M. Kabui <lewisemm@users.noreply.github.com>
2019-04-14 12:45:27 +0300

    Polish up the code
    - Use more elegant solution to create sub-folders inside train
    - Persist the category-to-folder map

--- train_subdirs.py
+++ train_subdirs.py
@@ -1,17 +1,19 @@
+import json
 import os
 import shutil
+
 import utils
 
-# categorize the photos into folders. Each folder represents a category
+# Categorize the photos into sub-folders.
+# Each sub-folder represents a category.
 filename = 'picnic_data/train.tsv'
 dir_prefix = 'picnic_data/train/'
 
 labels = utils.unique_category_labels(filename)
-len_labels = len(labels)
 label_to_folder_map = {}
 
 print("Creating train sub-directories...")
-for ind, label in zip(range(len_labels), labels):
+for ind, label in enumerate(labels):
     os.mkdir(dir_prefix + str(ind))
     label_to_folder_map[label] = ind
 
@@ -19,14 +21,18 @@ opened = open(filename, 'r')
 # skip the headers in the first line
 opened.readline()
 
-print("Moving photo files to appropriate subdirectories...")
+print("Moving photo files to sub-folders based on label...")
 for line in opened:
-    name, label = line.split('\t')
-    name = name.lower()
+    photo_filename, label = line.split('\t')
+    photo_filename = photo_filename.lower()
     label = label.lower()[:-1]
 
     dest = dir_prefix + str(label_to_folder_map[label])
-    name = dir_prefix + name
+    photo_filename = dir_prefix + photo_filename
         
     if label in label_to_folder_map:
-        shutil.move(name, dest)
+        shutil.move(photo_filename, dest)
+
+# persist the label to folder mapping
+with open('label_to_folder_map.json', 'w') as file:
+    file.write(json.dumps(label_to_folder_map)) 
