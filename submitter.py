1da49bf856102b6ce033b2789f58324a3a490915
Lewis M. Kabui <lewisemm@users.noreply.github.com>
2019-04-14 23:00:55 +0300

    Create file to generate final uploadable results

---
+++ submitter.py
@@ -0,0 +1,45 @@
+import os
+import shutil
+import utils
+
+from PIL import Image
+
+
+test_file = "picnic_data/test.tsv"
+dir_prefix = "picnic_data/test/"
+arch = "alexnet"
+checkpoint_path = "alexnet2.pth"
+
+opened = open(test_file, "r")
+opened.readline()
+
+sub = open("submission.tsv", "w")
+sub.write("file\tlabel\n")
+
+
+model = utils.load_checkpoint(arch, checkpoint_path)
+model.eval()
+
+for line in opened:
+    testfile_name = line[:-1]
+
+    top_probs, top_idx_names, top_categs = None, None, None
+
+    if '.png' in testfile_name:
+        # convert to jpg
+        im = Image.open(dir_prefix + testfile_name)
+        rgb_im = im.convert('RGB')
+
+        testfile_name_jpg = dir_prefix + testfile_name[:-4] + ".jpg"
+        
+        rgb_im.save(testfile_name_jpg)
+        top_probs, top_idx_names, top_categs = utils.predict(testfile_name_jpg, model, 1, 1)
+        os.remove(testfile_name_jpg)
+    else:
+        top_probs, top_idx_names, top_categs = utils.predict(dir_prefix + testfile_name, model, 1, 1)
+
+    completed_line = "{}\t{}\n".format(testfile_name, top_idx_names[0])
+    try:
+        sub.write(completed_line)
+    except Exception:
+        sub.close()    
